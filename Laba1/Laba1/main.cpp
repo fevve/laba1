1b9e011fad90ff957987633ad0a15223bdfb6d25
fevve <vorontsov0310@gmail.com>
2023-10-19 12:13:51 +0300

    Second commit:update

--- Laba1/Laba1/main.cpp
+++ Laba1/Laba1/main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 int main() {
-<<<<<<< HEAD
 	std::cout << "Hello world" << std::endl;
 	std::cout << "Second commit" << std::endl;
-=======
-	std::cout << "Hello world!!!" << std::endl;
->>>>>>> 1e5f8b273d54929329df0b8d632a0b024d907121
 }
